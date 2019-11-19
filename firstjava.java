30aebd8776334cd2fe93693403cd449346fc2a91
MAHESH897 <57609622+MAHESH897@users.noreply.github.com>
2019-11-19 09:07:58 +0530

    Update firstjava.java

--- firstjava.java
+++ firstjava.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class test {
+public class firstjava {
   public static void main(String[] args) {
     System.out.println("This is the test folder");
 
